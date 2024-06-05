0c2a087bc7d24affc9a285920547e28c4c53aa04
Mpino19 <mykmm19@gmail.com>
2024-06-05 14:15:27 +0900

    ノートの追加ボタンを実装

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Sidebar.css";
 
-const Sidebar = () => {
+const Sidebar = ({ onAddNote, notes }) => {
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
         <h1>ノート</h1>
-        <button>追加</button>
+        <button onClick={onAddNote}>追加</button>
       </div>
       <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div className="app-sidebar-note">
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button>削除</button>
+            </div>
+            <p>{note.content}</p>
+            <small>
+              {new Date(note.modDate).toLocaleDateString("ja-JP", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
         <div className="app-sidebar-note">
           <div className="sidebar-note-title">
             <strong>タイトル</strong>
